ab856773b547328de06af8e4c61eedc7d951a0c5
kbudd1234 <kevinpmbudd@gmail.com>
2015-11-11 16:52:34 -0600

    adds Map.init and Map.renderMap organization

--- build/js/app.js
+++ build/js/app.js
@@ -1,103 +1,78 @@
 // var googleAPIKey = AIzaSyBqKk_buo1F45vURvpqTQqZz4JyopNL9hQ;
 
+//data model of locations
 var locations = [
 	{
-		position: {lat: 34.0, lng: -118.25},
-    // map: map,
+		lat: 34.00,
+		lng: -118.25,
     title: 'Intelligentsia - Silverlake'
 	},
 	{
-		position: {lat: 34.1, lng: -118.25},
-    // map: map,
+		lat: 34.01,
+		lng: -118.25,
     title: 'Intelligentsia - Venice'
 	},
 	{
-		position: {lat: 34.2, lng: -118.25},
-    // map: map,
+		lat: 34.02,
+		lng: -118.25,
     title: 'Espresso Cielo'
 	},
 	{
-		position: {lat: 34.3, lng: -118.25},
-    // map: map,
+		lat: 34.03,
+		lng: -118.25,
     title: 'Blue Bottle Coffee'
 	},
 	{
-		position: {lat: 34.4, lng: -118.25},
-    // map: map,
+		lat: 34.04,
+		lng: -118.25,
     title: 'Intelligentsia - Pasadena'
 	}
-	];
+];
 
-var initMap = function() {
+var Map = {
 
-	var map;
+	init: function() {
+		var map;
+		this.renderMap();
+	},
 
-	map = new google.maps.Map(document.getElementById('map'), {
-				center: {lat: 34.0, lng: -118.25},
-				zoom: 11
-	});
+	renderMap: function() {
+
+		this.map = new google.maps.Map(document.getElementById('map'), {
+					center: {lat: 34.0, lng: -118.25},
+					zoom: 11
+		});
+
+	},
+
+	renderMarkers: function( location ) {
+		this.latitude = ko.observable(location.lat);
+		this.longitude = ko.observable(location.lng);
+		this.markerTitle = ko.observable(location.title);
+
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(latitude, longitude),
+			title: markerTitle,
+			map: map
+		});
+	}
+
 };
 
-// var Marker = function(data) {
-// 	var self = this;
-
-// 	this.position = ko.observable(data.position);
-// 	this.map = ko.observable(data.map);
-// 	this.title = ko.observable(data.title);
-
-// 	var marker = new google.maps.Marker( {
-// 		position: new google.maps.LatLng(self.position.lat, self.position.lng),
-// 		map: self.map,
-// 		title: self.title
-// 	});
-// }
-
-// ko.bindingHandlers.googlemap = {
-// 	init: function (element, valueAccessor) {
-// 		var
-// 		value = valueAccessor(),
-// 		mapOptions = {
-// 			zoom: 10,
-// 			center: new google.maps.LatLng(value.centerLat, value.centerLon),
-// 			mapTypeId: google.maps.MapTypeId.ROADMAP
-// 		},
-// 		map = new google.maps.Map(element, mapOptions);
-
-// 		for (var l in value.locations())
-// 		{
-// 			var latLng = new google.maps.LatLng(
-// 				value.locations()[l].position.lat,
-// 				value.locations()[l].position.lng);
-// 			var marker = new google.maps.Marker({
-// 				position: latLng,
-// 				map: map
-// 			});
-// 		}
-// 	}
-// };
 
 var ViewModel = function() {
 	var self = this;
 
-	this.locationList = ko.observableArray([]);
+	this.locationList = ko.observableArray( [] );
 
-	locations.forEach(function(item) {
+	locations.forEach(function( item ) {
 		self.locationList.push( item );
+
 	});
 
-	// var map;
-
-	// map = new google.maps.Map(document.getElementById('map'), {
-	// 			center: {lat: 34.0, lng: -118.25},
-	// 			zoom: 11
-	// });
-
-	// locationList = ko.observableArray([]);
-
-	// locations.forEach(function(item) {
-	// locationList.push( new Marker(item) );
-	// });
-
+	this.listviewClick = function ( location ) {
+		console.log( location.title );
+	}
 };
 
 ko.applyBindings(new ViewModel());
