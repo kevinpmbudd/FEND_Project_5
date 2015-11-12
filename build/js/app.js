149b405cc2ec86599c9d1e0a18cdf496a94d5aa3
kbudd1234 <kevinpmbudd@gmail.com>
2015-11-11 18:19:08 -0600

    adds Marker class

--- build/js/app.js
+++ build/js/app.js
@@ -1,61 +1,65 @@
 // var googleAPIKey = AIzaSyBqKk_buo1F45vURvpqTQqZz4JyopNL9hQ;
-
+var map;
 //data model of locations
 var locations = [
-	{
-		lat: 34.00,
-		lng: -118.25,
-    title: 'Intelligentsia - Silverlake'
-	},
-	{
-		lat: 34.01,
-		lng: -118.25,
-    title: 'Intelligentsia - Venice'
-	},
-	{
-		lat: 34.02,
-		lng: -118.25,
-    title: 'Espresso Cielo'
-	},
-	{
-		lat: 34.03,
-		lng: -118.25,
-    title: 'Blue Bottle Coffee'
-	},
-	{
-		lat: 34.04,
-		lng: -118.25,
-    title: 'Intelligentsia - Pasadena'
-	}
-];
+		{
+			lat: 34.00,
+			lng: -118.25,
+	    title: 'Intelligentsia - Silverlake'
+		},
+		{
+			lat: 34.01,
+			lng: -118.25,
+	    title: 'Intelligentsia - Venice'
+		},
+		{
+			lat: 34.02,
+			lng: -118.25,
+	    title: 'Espresso Cielo'
+		},
+		{
+			lat: 34.03,
+			lng: -118.25,
+	    title: 'Blue Bottle Coffee'
+		},
+		{
+			lat: 34.04,
+			lng: -118.25,
+	    title: 'Intelligentsia - Pasadena'
+		}
+	];
 
-var Map = {
+var initMap = function() {
 
-	init: function() {
-		var map;
-		this.renderMap();
-	},
-
-	renderMap: function() {
-
-		this.map = new google.maps.Map(document.getElementById('map'), {
+		map = new google.maps.Map(document.getElementById('map'), {
 					center: {lat: 34.0, lng: -118.25},
 					zoom: 11
 		});
 
-	},
+		// var marker = new google.maps.Marker({
+		// 	position: {lat: 34.0, lng: -118.25},
+		// 	map: map,
+		// 	title: "One"
+		// });
+};
 
-	renderMarkers: function( location ) {
-		this.latitude = ko.observable(location.lat);
-		this.longitude = ko.observable(location.lng);
-		this.markerTitle = ko.observable(location.title);
+var Marker = function( data ) {
+	this.lng = ko.observable( data.lng );
+	this.lat = ko.observable( data.lat );
+	this.title = ko.observable( data.title );
 
-		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng(latitude, longitude),
-			title: markerTitle,
-			map: map
-		});
-	}
+	console.log( data );
+	console.log( this.lng() );
+	console.log( this.lat() );
+	console.log( this.title() );
+
+	marker = new google.maps.Marker({
+		position: {lat: this.lat(), lng: this.lng()},
+		map: map,
+		title: this.title()
+	});
+
+	console.log(marker);
 
 };
 
@@ -65,14 +69,20 @@ var ViewModel = function() {
 
 	this.locationList = ko.observableArray( [] );
 
-	locations.forEach(function( item ) {
-		self.locationList.push( item );
-
+	locations.forEach( function ( location ) {
+		self.locationList.push( new Marker ( location ) );
 	});
+
+	this.currentLocation = ko.observable( this.locationList()[0] );
+
+	this.locationPicker = function( location ) {
+		self.currentLocation( location );
+	}
 
 	this.listviewClick = function ( location ) {
 		console.log( location.title );
-	}
+		Marker( location );
+	};
 };
 
 ko.applyBindings(new ViewModel());
