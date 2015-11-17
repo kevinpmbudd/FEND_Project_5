64486b723aeefd02a4a9b608f3d2211534a8efa8
kbudd1234 <kevinpmbudd@gmail.com>
2015-11-17 16:14:06 -0600

    adds link between listview click and markers. markers infowindows

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 var map,
-    loadMarkers,
-    refreshMarkers;
+    loadMarkers;
 
 var markers = [];
 //data model of locations
@@ -49,8 +48,8 @@ var initMap = function() {
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 34.0,
-            lng: -118.25
+            lat: 34.07,
+            lng: -118.30
         },
         zoom: 11,
         zoomControl: true,
@@ -66,42 +65,15 @@ var initMap = function() {
 
 
 var renderMarkers = function( locations ) {
-    var data;
+    locations.forEach(function(data) {
+        // data = locations[i];
+        console.log(data.title);
 
-    var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
+        var contentString = '<div id="content">' + data.title + '</div>';
 
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString
-      });
-
-    for (var i = 0; locations.length; i ++) {
-
-        data = locations[i];
-
-        // console.log(data);
-
-        // this.lat = ko.observable(data.lat);
-        // this.lng = ko.observable(data.lng);
-        // this.title = ko.observable(data.title);
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
 
         var marker = new google.maps.Marker({
             position: {
@@ -115,11 +87,11 @@ var renderMarkers = function( locations ) {
         });
 
         marker.addListener('click', function() {
-            infowindow.open(map,marker);
+            infowindow.open(marker.get('map'), marker);
         });
 
         markers.push(marker);
-    }
+    });
 };
 
 // Sets the map on all markers in the array.
@@ -139,14 +111,12 @@ var deleteMarkers = function() {
 };
 
 var setVisibilty = function( filteredLocations ) {
-    // console.log(filteredLocations);
-
     for (var i = 0; i < markers.length; i++) {
 
 
         for (var j = 0; j < filteredLocations.length; j++) {
             var filteredMark = filteredLocations[j];
-            console.log(filteredMark.title);
+            // console.log(filteredMark.title);
             // console.log(markers[i]);
             if (filteredMark.title == markers[i].title) {
                 markers[i].setVisible(true);
@@ -156,14 +126,20 @@ var setVisibilty = function( filteredLocations ) {
                 markers[i].setVisible(false);
         }
     }
+
+    if (filteredLocations.length == 0) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setVisible(false);
+        }
+    }
 };
 
-ko.utils.stringStartsWith = function(string, startsWith) {
-    string = string || "";
-    if (startsWith.length > string.length)
-    	return false;
-    return string.substring(0, startsWith.length) === startsWith;
-};
+// ko.utils.stringStartsWith = function(string, startsWith) {
+//     string = string || "";
+//     if (startsWith.length > string.length)
+//     	return false;
+//     return string.substring(0, startsWith.length) === startsWith;
+// };
 
 var ViewModel = function() {
     var self = this;
@@ -193,15 +169,16 @@ var ViewModel = function() {
     });
 
     this.listviewClick = function(location) {
-        // console.log(location.title);
+        console.log(location.title);
+        markers.forEach(function( marker ) {
+            if (marker.title == location.title) {
+                marker.setAnimation(google.maps.Animation.DROP);
+            }
+        });
     };
 
     loadMarkers = function () {
         renderMarkers(self.search());
-    };
-
-    refreshMarkers = function () {
-        setVisibilty(self.search());
     };
 
 };
@@ -211,9 +188,3 @@ ko.applyBindings(new ViewModel());
 $(document).ready(function() {
     loadMarkers();
 });
-
-// $('#search-box').keypress(function () {
-//     // deleteMarkers();
-//     // loadMarkers();
-//     refreshMarkers();
-// });
