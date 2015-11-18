05228b7cb7ffabbad6ef98eaba06f9b3b860ab80
kbudd1234 <kevinpmbudd@gmail.com>
2015-11-18 17:53:29 -0600

    adds ajax yelp call.

--- build/js/app.js
+++ build/js/app.js
@@ -2,55 +2,49 @@
 var map,
     loadMarkers;
 
-var CLIENT_ID = 'OUJ3QNUKM33CWHU4G4VIXM5IXPZLNCXWMBUSWFSSA1BQW4YT',
-    CLIENT_SECRET = '42DDHVI3XBNIOB1T4QL5P3MD3HQK5YLP5XJRWEZXQB04D31C',
-    foursquareURL = 'https://api.foursquare.com/v2/venues/search?client_id='
-        + CLIENT_ID + ' &client_secret=' + CLIENT_SECRET + '&v=20130815&ll=34.09,-118.28&query=coffee';
-
-var id = '49bad052f964a520b3531fe3';
-var foursquarePhotosURL = 'https://api.foursquare.com/v2/venues/' + id + '/photos';
-
 var markers = [];
 //data model of locations
 var locations = [{
     lat: 34.091966,
     lng: -118.279965,
-    title: 'Intelligentsia - Silverlake'
+    title: 'Intelligentsia - Silverlake',
+    yelpID: 'intelligentsia-coffee-los-angeles-4'
 }, {
     lat: 33.991111,
     lng: -118.466812,
-    title: 'Intelligentsia - Venice'
+    title: 'Intelligentsia - Venice',
+    yelpID: 'intelligentsia-coffee-venice'
 }, {
     lat: 33.998110,
     lng: -118.479512,
-    title: 'Espresso Cielo'
+    title: 'Espresso Cielo',
+    yelpID: 'espresso-cielo-santa-monica'
 }, {
     lat: 34.038812,
     lng: -118.232656,
-    title: 'Blue Bottle Coffee - Arts District'
+    title: 'Blue Bottle Coffee - Arts District',
+    yelpID: 'blue-bottle-coffee-los-angeles-12'
 }, {
     lat: 33.992004,
     lng: -118.470488,
-    title: 'Blue Bottle Coffee - Abbot Kinney'
+    title: 'Blue Bottle Coffee - Abbot Kinney',
+    yelpID: 'blue-bottle-los-angeles-3'
 }, {
     lat: 34.076009,
     lng: -118.370111,
-    title: 'Blue Bottle Coffee - Beverly Grove Cafe'
+    title: 'Blue Bottle Coffee - Beverly Grove Cafe',
+    yelpID: 'blue-bottle-coffee-los-angeles-14'
 }, {
     lat: 34.078993,
     lng: -118.309309,
-    title: 'LaB - Coffee and Roasters'
+    title: 'LaB - Coffee and Roasters',
+    yelpID: 'lab-coffee-and-roasters-los-angeles'
 }, {
     lat: 34.145864,
     lng: -118.151678,
-    title: 'Intelligentsia - Pasadena'
+    title: 'Intelligentsia - Pasadena',
+    yelpID: 'intelligentsia-coffee-pasadena'
 }];
-
-var Location = function ( lat, lng, title ) {
-	this.lat = lat;
-	this.lng = lng;
-	this.title = title;
-};
 
 var initMap = function() {
 
@@ -71,20 +65,72 @@ var initMap = function() {
     });
 };
 
-// https://api.foursquare.com/v2/venues/search
-//   ?client_id=CLIENT_ID
-//   &client_secret=CLIENT_SECRET
-//   &v=20130815
-//   &ll=40.7,-74
-//   &query=sushi
+// window.addEventListener('load',function(){
+//       if(document.getElementById('map-canvas')){
+//         google.load("maps", "3",{
+//           callback:function(){
+//              map = new google.maps.Map(document.getElementById('map-canvas'), {
+//                 center: new google.maps.LatLng(0,0),
+//                 zoom: 3
+//               });
+//              loadMarkers();
+//           }
+//         });
+//       }
+// },false);
 
 
 var renderMarkers = function( locations ) {
-    locations.forEach(function(data) {
+    locations.forEach(function( data ) {
         // data = locations[i];
-        console.log(data.title);
+        // console.log(data.title);
 
-        var contentString = '<div id="content">' + data.title + '</div>';
+        var contentString;
+
+        var yelp = function(id) {
+
+            var YELP_CONSUMER_KEY = 'Q9ytXgv7OcR2aC2HYDsGPg',
+                YELP_CONSUMER_SECRET = 'MlUPkhWTf-2ar9ZWhnNlRb1PDZg',
+                YELP_TOKEN = 'KdkGSp3vHhxzajyR2sNfkpY15m53MaSV',
+                YELP_TOKEN_SECRET = 'fUufpxoSWMiwutlBmKs9x5_ck8o',
+                YELP_ROOT_URL = 'https://api.yelp.com/v2/business/';
+
+            function nonce_generate() {
+              return (Math.floor(Math.random() * 1e12).toString());
+            }
+
+            var yelp_url = YELP_ROOT_URL + id;
+
+                var parameters = {
+                  oauth_consumer_key: YELP_CONSUMER_KEY,
+                  oauth_token: YELP_TOKEN,
+                  oauth_nonce: nonce_generate(),
+                  oauth_timestamp: Math.floor(Date.now()/1000),
+                  oauth_signature_method: 'HMAC-SHA1',
+                  oauth_version : '1.0',
+                  callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+                };
+
+                var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, YELP_CONSUMER_SECRET, YELP_TOKEN_SECRET);
+                parameters.oauth_signature = encodedSignature;
+
+                var settings = {
+                  url: yelp_url,
+                  data: parameters,
+                  cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+                  dataType: 'jsonp',
+                  fail: function() {
+                    console.log('ajax error');
+                  }
+                };
+
+                // Send AJAX query via jQuery library.
+                $.ajax(settings).done(function (data) {
+                    contentString = '<div id="content">' + data.rating +'<br>' + data.review_count + '</div>';
+                });
+        };
+
+        yelp( data.yelpID );
 
         var infowindow = new google.maps.InfoWindow({
           content: contentString
@@ -110,20 +156,20 @@ var renderMarkers = function( locations ) {
 };
 
 // Sets the map on all markers in the array.
-var setMapOnAll = function(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-};
+// var setMapOnAll = function(map) {
+//   for (var i = 0; i < markers.length; i++) {
+//     markers[i].setMap(map);
+//   }
+// };
 
-var clearMarkers = function() {
-  setMapOnAll(null);
-};
+// var clearMarkers = function() {
+//   setMapOnAll(null);
+// };
 
-var deleteMarkers = function() {
-  clearMarkers();
-  markers = [];
-};
+// var deleteMarkers = function() {
+//   clearMarkers();
+//   markers = [];
+// };
 
 var setVisibilty = function( filteredLocations ) {
     for (var i = 0; i < markers.length; i++) {
@@ -149,12 +195,8 @@ var setVisibilty = function( filteredLocations ) {
     }
 };
 
-// ko.utils.stringStartsWith = function(string, startsWith) {
-//     string = string || "";
-//     if (startsWith.length > string.length)
-//     	return false;
-//     return string.substring(0, startsWith.length) === startsWith;
-// };
+////////////////////////////////////////////////////////////////////
+////  YELP API  ///////////////////////////////////////////////////
 
 var ViewModel = function() {
     var self = this;
@@ -191,11 +233,7 @@ var ViewModel = function() {
             }
         });
 
-        $.ajax({
-            url: foursquarePhotosURL
-        }).done(function( data ) {
-            console.log( data );
-        });
+        // yelp(location.yelpID);
     };
 
     loadMarkers = function () {
