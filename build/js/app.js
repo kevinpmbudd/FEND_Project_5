1991f7d972eb4672184e8a913de9486a71ad2884
kbudd1234 <kevinpmbudd@gmail.com>
2015-11-19 16:06:19 -0600

    adds some comments

--- build/js/app.js
+++ build/js/app.js
@@ -1,32 +1,20 @@
 'use strict';
+////////////////////////////////////////////////////////////////////
+////  GLOBAL VARIABLES ////////////////////////////////////////////
 var map,
-    loadMarkers;
+    infowindow,
+    markers = [];
 
-var markers = [];
-//data model of locations
+////////////////////////////////////////////////////////////////////
+////  DATA MODEL  /////////////////////////////////////////////////
+///
+// # Store each location to be featured on the map as an object in
+//      in the locations array.
 var locations = [{
-    lat: 34.091966,
-    lng: -118.279965,
-    title: 'Intelligentsia - Silverlake',
-    yelpID: 'intelligentsia-coffee-los-angeles-4',
-    yelpRatingImg: null,
-    yelpRating: null,
-    yelpReviewCount: null,
-    marker: null
-}, {
-    lat: 33.991111,
-    lng: -118.466812,
-    title: 'Intelligentsia - Venice',
-    yelpID: 'intelligentsia-coffee-venice',
-    yelpRatingImg: null,
-    yelpRating: null,
-    yelpReviewCount: null,
-    marker: null
-}, {
-    lat: 33.998110,
-    lng: -118.479512,
-    title: 'Espresso Cielo',
-    yelpID: 'espresso-cielo-santa-monica',
+    lat: 33.992004,
+    lng: -118.470488,
+    title: 'Blue Bottle Coffee - Abbot Kinney',
+    yelpID: 'blue-bottle-los-angeles-3',
     yelpRatingImg: null,
     yelpRating: null,
     yelpReviewCount: null,
@@ -41,19 +29,19 @@ var locations = [{
     yelpReviewCount: null,
     marker: null
 }, {
-    lat: 33.992004,
-    lng: -118.470488,
-    title: 'Blue Bottle Coffee - Abbot Kinney',
-    yelpID: 'blue-bottle-los-angeles-3',
+    lat: 34.076009,
+    lng: -118.370111,
+    title: 'Blue Bottle Coffee - Beverly Grove Cafe',
+    yelpID: 'blue-bottle-coffee-los-angeles-14',
     yelpRatingImg: null,
     yelpRating: null,
     yelpReviewCount: null,
     marker: null
 }, {
-    lat: 34.076009,
-    lng: -118.370111,
-    title: 'Blue Bottle Coffee - Beverly Grove Cafe',
-    yelpID: 'blue-bottle-coffee-los-angeles-14',
+    lat: 33.998110,
+    lng: -118.479512,
+    title: 'Espresso Cielo',
+    yelpID: 'espresso-cielo-santa-monica',
     yelpRatingImg: null,
     yelpRating: null,
     yelpReviewCount: null,
@@ -76,10 +64,32 @@ var locations = [{
     yelpRating: null,
     yelpReviewCount: null,
     marker: null
+}, {
+    lat: 34.091966,
+    lng: -118.279965,
+    title: 'Intelligentsia - Silverlake',
+    yelpID: 'intelligentsia-coffee-los-angeles-4',
+    yelpRatingImg: null,
+    yelpRating: null,
+    yelpReviewCount: null,
+    marker: null
+}, {
+    lat: 33.991111,
+    lng: -118.466812,
+    title: 'Intelligentsia - Venice',
+    yelpID: 'intelligentsia-coffee-venice',
+    yelpRatingImg: null,
+    yelpRating: null,
+    yelpReviewCount: null,
+    marker: null
 }];
 
+////////////////////////////////////////////////////////////////////
+////  MAP INITIALIZATION  /////////////////////////////////////////
+///
+// # Instantiate the google map with correct coordinates.
+// # Instantiate the single infowindow to be re-used by each marker.
 var initMap = function() {
-    loadYelp();
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -87,25 +97,24 @@ var initMap = function() {
             lng: -118.30
         },
         zoom: 11,
-        zoomControl: true,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     });
 
-    google.maps.event.addDomListener(window, 'resize', function() {
-        var center = map.getCenter();
-        google.maps.event.trigger(map, 'resize');
-        map.setCenter(center);
-    });
+    infowindow = new google.maps.InfoWindow();
 };
 
-var renderMarker = function( location ) {
+////////////////////////////////////////////////////////////////////
+////  MAP MARKERS INITIALIZATION  /////////////////////////////////
+///
+// # Each location is passed in to build its map marker.
+// # Create a content string with data sourced from Yelp API.
+// # Instantiate a new marker for each location
+// # Add click listener for each marker to open its infowindow
+// # Store each marker in markers array as well as in locations array
+var initMarker = function(location) {
 
     var contentString = '<div>' + location.title + '<br> Yelp Rating: <img src="' + location.yelpRatingImg + '">' + '<br>' +
-      location.yelpRating + ' stars<br>Number of Reviews: ' + location.yelpReviewCount +'</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
+        location.yelpRating + ' stars<br>Number of Reviews: ' + location.yelpReviewCount + '</div>';
 
     var marker = new google.maps.Marker({
         position: {
@@ -119,16 +128,21 @@ var renderMarker = function( location ) {
     });
 
     marker.addListener('click', function() {
+        infowindow.setContent(contentString);
         infowindow.open(marker.get('map'), marker);
     });
 
     location.marker = marker;
-
+    markers.push(marker);
 };
 
 ////////////////////////////////////////////////////////////////////
 ////  YELP API  ///////////////////////////////////////////////////
-var yelp = function( location ) {
+//
+// # Store constants to generate OAUTH encoded signature.
+// # Use $.ajax to and its done function to retrieve Yelp data for
+//      each location.
+var yelp = function(location) {
 
     var YELP_CONSUMER_KEY = 'Q9ytXgv7OcR2aC2HYDsGPg',
         YELP_CONSUMER_SECRET = 'MlUPkhWTf-2ar9ZWhnNlRb1PDZg',
@@ -137,90 +151,91 @@ var yelp = function( location ) {
         YELP_ROOT_URL = 'https://api.yelp.com/v2/business/';
 
     function nonce_generate() {
-      return (Math.floor(Math.random() * 1e12).toString());
+        return (Math.floor(Math.random() * 1e12).toString());
     }
 
     var yelp_url = YELP_ROOT_URL + location.yelpID;
 
-        var parameters = {
-          oauth_consumer_key: YELP_CONSUMER_KEY,
-          oauth_token: YELP_TOKEN,
-          oauth_nonce: nonce_generate(),
-          oauth_timestamp: Math.floor(Date.now()/1000),
-          oauth_signature_method: 'HMAC-SHA1',
-          oauth_version : '1.0',
-          callback: 'cb'              // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
-        };
+    var parameters = {
+        oauth_consumer_key: YELP_CONSUMER_KEY,
+        oauth_token: YELP_TOKEN,
+        oauth_nonce: nonce_generate(),
+        oauth_timestamp: Math.floor(Date.now() / 1000),
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_version: '1.0',
+        callback: 'cb'
+    };
 
-        var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, YELP_CONSUMER_SECRET, YELP_TOKEN_SECRET);
-        parameters.oauth_signature = encodedSignature;
+    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, YELP_CONSUMER_SECRET, YELP_TOKEN_SECRET);
+    parameters.oauth_signature = encodedSignature;
 
-        var settings = {
-          url: yelp_url,
-          data: parameters,
-          cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
-          dataType: 'jsonp',
-          fail: function() {
-            console.log('ajax error');
-          }
-        };
+    var settings = {
+        url: yelp_url,
+        data: parameters,
+        cache: true,
+        dataType: 'jsonp',
+        fail: function() {
+            location.yelpRating = "Yelp rating not currently available";
+            location.yelpReviewCount = "Review count not currently available";
+            location.yelpRatingImg = "./img/coffee.png";
+            initMarker(location);
+        }
+    };
 
-        // Send AJAX query via jQuery library.
-        $.ajax(settings).done(function ( data ) {
-            location.yelpRating = data.rating;
-            location.yelpReviewCount = data.review_count;
-            location.yelpRatingImg = data.rating_img_url;
-            console.log(data.rating_img_url);
-            renderMarker ( location );
-        });
-
-
+    $.ajax(settings)
+    .done(function(data) {
+        location.yelpRating = data.rating;
+        location.yelpReviewCount = data.review_count;
+        location.yelpRatingImg = data.rating_img_url;
+        initMarker(location);
+    })
+    .fail(function() {
+        location.yelpRating = "Yelp rating not currently available";
+        location.yelpReviewCount = "Review count not currently available";
+        location.yelpRatingImg = "./img/coffee.png";
+        initMarker(location);
+    });
 };
 
-var setVisibilty = function( filteredLocations ) {
+////////////////////////////////////////////////////////////////////
+////  SET THE MARKERS VISIBILITY  /////////////////////////////////
+//
+// # Recieve filtered array of locations.
+// # Cross reference filtered arrray with all inclusive locations arrary.
+// # Set visible to true for locations in the filtered array and
+//      all other locations to false.
+var setVisibilty = function(filteredLocations) {
     for (var i = 0; i < markers.length; i++) {
-
-
         for (var j = 0; j < filteredLocations.length; j++) {
             var filteredMark = filteredLocations[j];
-            if (filteredMark.title == markers[i].title) {
+            if (filteredMark.title === markers[i].title) {
                 markers[i].setVisible(true);
                 break;
-            }
-            else
+            } else {
                 markers[i].setVisible(false);
-        }
-    }
-
-    if (filteredLocations.length == 0) {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setVisible(false);
+            }
         }
     }
 };
 
-
+////////////////////////////////////////////////////////////////////
+////  KNOCKOUT VIEW MODEL /////////////////////////////////////////
+//
+// # Recieve filtered array of locations.
+// # Cross reference filtered arrray with all inclusive locations arrary.
+// # Set visible to true for locations in the filtered array and
+//      all other locations to false.
 var ViewModel = function() {
     var self = this;
 
-    var compare = function(a,b) {
-  		if (a.title < b.title)
-    		return -1;
-  		if (a.title > b.title)
-    		return 1;
-  		return 0;
-	}
-
-	locations.sort(compare);
-
-    self.locationList = ko.observableArray( locations );
+    self.locationList = ko.observableArray(locations);
 
     self.query = ko.observable('');
 
-    self.search = ko.computed( function() {
-        return ko.utils.arrayFilter(self.locationList(), function( location ) {
+    self.search = ko.computed(function() {
+        return ko.utils.arrayFilter(self.locationList(), function(location) {
             return location.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
-         });
+        });
     });
 
     self.search.subscribe(function() {
@@ -231,17 +246,12 @@ var ViewModel = function() {
         location.marker.setAnimation(google.maps.Animation.DROP);
         google.maps.event.trigger(location.marker, 'click');
     };
-
-    loadMarkers = function () {
-        renderMarkers(self.search());
-    };
-
 };
 
 var loadYelp = function() {
-    locations.forEach(function ( location ) {
-        yelp( location );
-    })
+    locations.forEach(function(location) {
+        yelp(location);
+    });
 };
 
 ko.applyBindings(new ViewModel());
